c76896effb376c47c522262af06e89194e4bc089
Louviers Joseph <josephlouviers@gmail.com>
2025-03-09 06:14:13 +0000

    FinishController

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,16 +1,18 @@
 package com.example.controller;
 import java.sql.SQLException;
-
+import org.jboss.logging.Messages;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.entity.*;
 import com.example.service.*;
 
@@ -21,10 +23,10 @@ import com.example.service.*;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-@Controller
+@RestController
 public class SocialMediaController {
     @PostMapping("/register")
-    public @ResponseBody ResponseEntity<Account> addAccount(@RequestBody Account account){
+    public ResponseEntity<Account> addAccount(@RequestBody Account account){
         if(account.getUsername().length() > 0 && account.getPassword().length() >= 4)
             try{
                 AccountService.addAccount(account);
@@ -37,9 +39,49 @@ public class SocialMediaController {
     }
 
     @PostMapping("/login")
-    public @ResponseBody ResponseEntity<Account> login(@RequestBody Account account){
+    public ResponseEntity<Account> login(@RequestBody Account account){
         if(AccountService.login(account)!=null)
             return ResponseEntity.ok(AccountService.login(account));
         return ResponseEntity.status(401).build();
     }
+
+    @PostMapping("/messages")
+    public ResponseEntity<Message> addMessage(@RequestBody Message message){
+        int message_length = message.getMessageText().length();
+
+        if(message_length > 0 && message_length < 255){
+            MessageService.addMessage(message);
+            return ResponseEntity.ok(message);
+        }
+    }
+
+    @GetMapping("/messages")
+    public ResponseEntity<Message[]> getAllMessages()
+        {return ResponseEntity.ok(MessageService.getAllMessages());}
+
+    @GetMapping("/messages/{message_id}")
+    public ResponseEntity<Message> getMessage(@PathVariable("message_id") int id)
+        {return ResponseEntity.ok(MessageService.getMessage(id));}
+
+    @DeleteMapping("/messages/{message_id}")
+    public ResponseEntity<Integer> deleteMessage(@PathVariable("message_id") int id ){
+        int rows_deleted = MessageService.deleteMessage(id);
+        return rows_deleted > 0 ? ResponseEntity.ok(rows_deleted) : ResponseEntity.ok().build(); 
+    }
+
+    @PatchMapping("/messages/{message_id}")
+    public ResponseEntity<Integer> updateMessage(@RequestBody Message message, @PathVariable("message_id") int id){
+        int rows_updated = MessageService.updateMessage(message, id);
+        int message_length = message.getMessageText().length();
+
+        if(rows_updated > 0 && message_length > 0 && message_length < 255)
+            return ResponseEntity.ok(rows_updated);
+
+        return ResponseEntity.status(400).build();
+    }
+
+    @GetMapping("/accounts/{account_id}/messages")
+    public ResponseEntity<Message[]> getMessagesFromUser(@PathVariable("account_id") int id)
+        {return ResponseEntity.ok(AccountService.getMessagesFromUser(id));}
+
 }
